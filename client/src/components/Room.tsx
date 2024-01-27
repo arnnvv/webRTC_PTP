b11088aeb34de222db6b1d8eaaf5b702271e3c0b
arnnvv <arnav.900@protonmail.com>
2024-01-28 02:09:21 +0530

    chkPt

--- client/src/components/Room.tsx
+++ client/src/components/Room.tsx
@@ -1,21 +1,63 @@
-import { nameAtom, socketAtom } from "../store/atoms";
+import {
+  nameAtom,
+  socketAtom,
+  lobbyAtom,
+  sendingPcAtom,
+  receivingPcAtom,
+  remoteVideoTrackAtom,
+  remoteAudioTrackAtom,
+  remoteMediaStreamAtom,
+} from "../store/atoms";
 import { useRecoilState } from "recoil";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Socket, io } from "socket.io-client";
 
 const URL: string = "http://localhost:3000";
 const Room = () => {
   const name = useRecoilState(nameAtom);
-  const [socket, setSocket] = useRecoilState(socketAtom);
+  const [setSocket] = useRecoilState(socketAtom);
+  const [setLobby] = useRecoilState(lobbyAtom);
+  const [sendingPc, setSendingPc] = useRecoilState(sendingPcAtom);
+  const [receivingPc, setReceivingPc] = useRecoilState(receivingPcAtom);
+  const [remoteVideoTrack, setRemoteVideoTrack] =
+    useRecoilState(remoteVideoTrackAtom);
+  const [remoteAudioTrack, setRemoteAudioTrack] =
+    useRecoilState(remoteAudioTrackAtom);
+  const [remoteMediaStream, setRemoteMediaStream] = useRecoilState(
+    remoteMediaStreamAtom,
+  );
+  const remoteVideoRef = useRef<HTMLVideoElement>();
+  const localVideoRef = useRef<HTMLVideoElement>();
   useEffect(() => {
-    const socket = io(URL, {
-      autoConnect: false,
-    });
-    socket.on("send-offer", () => {
+    const socket: Socket = io(URL);
+    socket.on("send-offer", async ({ roomId }) => {
       alert(`Send Offer Plz`);
+      setLobby(false);
+      const pc = new RTCPeerConnection();
+      setSendingPc(pc);
+      socket.emit("offer", {
+        sdp: "",
+        roomId,
+      });
+    });
+
+    socket.on("offer", ({ roomId, offer }) => {
+      alert(`Send Answer Plz`);
+      socket.emit("answer", {
+        roomId,
+        sdp: "",
+      });
+    });
+
+    socket.on("answer", ({ roomId, answer }) => {
+      alert(`Connected`);
+    });
+
+    socket.on("lobby", () => {
+      setLobby(true);
     });
     setSocket(socket);
-  }, [name, setSocket]);
+  }, [name]);
   return <>Hi {name}</>;
 };
 
--- client/src/store/atoms.ts
+++ client/src/store/atoms.ts
@@ -10,3 +10,33 @@ export const socketAtom = atom<Socket | null>({
   key: "socketAtom",
   default: null,
 });
+
+export const lobbyAtom = atom({
+  key: "lobbyAtom",
+  default: true,
+});
+
+export const sendingPcAtom = atom<null | RTCPeerConnection>({
+  key: "sendingPcAtom",
+  default: null,
+});
+
+export const receivingPcAtom = atom<null | RTCPeerConnection>({
+  key: "receivingPcAtom",
+  default: null,
+});
+
+export const remoteVideoTrackAtom = atom<MediaStreamTrack | null>({
+  key: "remoteVideoTrackAtom",
+  default: null,
+});
+
+export const remoteAudioTrackAtom = atom<MediaStreamTrack | null>({
+  key: "remoteAudioTrackAtom",
+  default: null,
+});
+
+export const remoteMediaStreamAtom = atom<MediaStream | null>({
+  key: "remoteMediaStreamAtom",
+  default: null,
+});
